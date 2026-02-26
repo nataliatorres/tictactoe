a5303ae76aef0a17a2f7c484d98a201689ee399c
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-26 14:57:57 +1000

    Adding the GameSquare component and styling it

---
+++ src/components/GameSquare.jsx
@@ -0,0 +1,14 @@
+const GameSquare = ({children, squareStyle, isSelected, updateBoard, index}) => {
+
+const handleClick = () => {
+    updateBoard()
+}
+
+    return (
+        <div onClick={handleClick} className={` ${squareStyle ? squareStyle : 'grid w-25 h-25 border-2 rounded-[5px] border-solid border-white place-items-center cursor-pointer text-5xl'} ${isSelected ? 'color-black bg-[#7baed0]' : ''}`}>
+            {children}
+        </div>
+    )
+}
+
+export default GameSquare;
