5211fd2410095a80cc01b1b103124f32400da88f
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-26 16:36:25 +1000

    Adding the logic to show the elements on the board, update it and check for a winner.

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,57 @@ import './App.css'
 import GameSquare from './components/GameSquare'
 
 const TURNS = {
-  X: 'x',
-  O: 'o'
+  X: '❌',
+  O: '⚪'
 }
 
-function App() {
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
-  const board = Array(9).fill(null)
+function App() {
+  const turnSquareStyle = "grid w-17.5 h-17.5 border-2 rounded-[5px] border-solid border-transparent place-items-center pointer-events-none text-5xl"
+
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  )
 
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
 
-  const turnSquareStyle="grid w-17.5 h-17.5 border-2 rounded-[5px] border-solid border-transparent place-items-center pointer-events-none text-5xl"
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck [c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+}
 
-  const updateBoard =() => {
+  const updateBoard = (index) => {
+    if(board[index] || winner) return
 
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   return (
@@ -33,6 +70,7 @@ function App() {
               index={index}
               updateBoard={updateBoard}
               >
+              {board[index]}
               </GameSquare>
             )
           })
