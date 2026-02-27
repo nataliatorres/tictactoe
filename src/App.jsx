e8eae8357e71b90af84943d18ca055479c76dd6d
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-27 16:26:42 +1000

    Extracted a function

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import './App.css'
 import GameSquare from './components/GameSquare'
 import confetti from 'canvas-confetti'
 import { TURNS } from './utils/constants'
-import { checkWinnerFrom } from './logic/board'
+import { checkWinnerFrom, checkEndGame } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
 
 function App() {
   const turnSquareStyle = "grid w-17.5 h-17.5 border-2 rounded-[5px] border-solid border-transparent place-items-center pointer-events-none text-5xl"
@@ -42,10 +43,6 @@ function App() {
     setWinner(null)
   }
 
-  const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square !== null)
-  }
-
   return (
     <main className='w-fit my-10 mx-auto text-center'>
       <h1>Tic Tac Toe</h1>
@@ -73,29 +70,7 @@ function App() {
         <GameSquare squareStyle={turnSquareStyle} isSelected={turn === TURNS.O}>{TURNS.O}</GameSquare>
       </section>
       
-      {
-        winner !== null && (
-          <section className='grid absolute place-items-center w-screen h-screen top-0 left-0 bg-[rgba(0,0,0,0.7)]'>
-            <div className='flex flex-col justify-center items-center gap-5 w-80 h-75 bg-[#111] border-2 border-solid border-gray-200 rounded-xl'>
-              <h2>
-                {
-                  winner === false ? 'Tie' : 'Winner'
-                }
-              </h2>
-
-              <header className='flex gap-3.5 w-fit my-0 mx-auto rounded-xl'>
-                {
-                  winner && <GameSquare squareStyle={`w-17.5 h=17.5 pointer-events-none border-transparent`}>{winner}</GameSquare>
-                }
-              </header>
-
-              <footer>
-                <button onClick={resetGame} className='w-25 py-2 px-3 m-6 bg-transparent text-gray-200 border-2 border-solid border-gray-200 rounded-lg transition-200 font-bold cursor-pointer hover:bg-[#eee] hover:text-[#222]'>Play again</button>
-              </footer>
-            </div>
-          </section>
-        )
-      }
+      <WinnerModal winner={winner} resetGame={resetGame}></WinnerModal>
     </main>
   )
 }
