dc7706af110038f70c91b30471c941f41fe6b401
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-27 14:17:18 +1000

    Adding the logic to reset the game and check if the game has ended

--- src/App.jsx
+++ src/App.jsx
@@ -55,12 +55,26 @@ function App() {
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
       setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
     }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null)
   }
 
   return (
     <main className='w-fit my-10 mx-auto text-center'>
-      <h1 className='mb-4'>Placeholder</h1>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame} className='w-25 py-2 px-3 m-6 bg-transparent text-gray-200 border-2 border-solid border-gray-200 rounded-lg transition-200 font-bold cursor-pointer hover:bg-[#eee] hover:text-[#222]'>Reset game</button>
+
       <section className='grid grid-cols-3 gap-2.5'>
         {
           board.map((_, index) => {
@@ -81,6 +95,30 @@ function App() {
         <GameSquare squareStyle={turnSquareStyle} isSelected={turn === TURNS.X}>{TURNS.X}</GameSquare>
         <GameSquare squareStyle={turnSquareStyle} isSelected={turn === TURNS.O}>{TURNS.O}</GameSquare>
       </section>
+      
+      {
+        winner !== null && (
+          <section className='grid absolute place-items-center w-screen h-screen top-0 left-0 bg-[rgba(0,0,0,0.7)]'>
+            <div className='flex flex-col justify-center items-center gap-5 w-80 h-75 bg-[#111] border-2 border-solid border-gray-200 rounded-xl'>
+              <h2>
+                {
+                  winner === false ? 'Tie' : 'Winner'
+                }
+              </h2>
+
+              <header className='flex gap-3.5 w-fit my-0 mx-auto rounded-xl'>
+                {
+                  winner && <GameSquare squareStyle={`w-17.5 h=17.5 pointer-events-none border-transparent`}>{winner}</GameSquare>
+                }
+              </header>
+
+              <footer>
+                <button onClick={resetGame} className='w-25 py-2 px-3 m-6 bg-transparent text-gray-200 border-2 border-solid border-gray-200 rounded-lg transition-200 font-bold cursor-pointer hover:bg-[#eee] hover:text-[#222]'>Play again</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
