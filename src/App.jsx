8560b3e1eeeab71b55fe90ab2d85effae2d68d5d
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-03-03 15:34:56 +1000

    Importing the NavBar component and updating the style for the main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar'
 import GameSquare from './components/GameSquare'
 import confetti from 'canvas-confetti'
 import { TURNS } from './utils/constants'
 import { checkWinnerFrom, checkEndGame } from './logic/board'
 import { WinnerModal } from './components/WinnerModal'
 import { saveGameToStorage, resetGameStorage } from './logic'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
 
 function App() {
   const turnSquareStyle = "grid w-17.5 h-17.5 border-2 rounded-[5px] border-solid border-transparent place-items-center pointer-events-none text-5xl"
@@ -55,33 +63,38 @@ function App() {
   }
 
   return (
-    <main className='w-fit my-10 mx-auto text-center'>
-      <h1 className='my-5 text-[#4f5030]'>Tic Tac Toe</h1>
-      <section className='grid grid-cols-3 gap-2.5'>
-        {
-          // board.map((_, index) => {
-          board.map((square, index) => {
-            return (
-              <GameSquare
-              key={index}
-              index={index}
-              updateBoard={updateBoard}
-              >
-              {square}
-              </GameSquare>
-            )
-          })
-        }
-      </section>
+    <>
+      <NavBar/>
+      <main className='w-86 flex justify-center my-10 mx-auto text-center'>
+        <section>
+          <h1 className='my-5 text-[#4f5030]'>Tic Tac Toe</h1>
+          <section className='w-[320px] grid grid-cols-3 gap-2.5'>
+            {
+              // board.map((_, index) => {
+              board.map((square, index) => {
+                return (
+                  <GameSquare
+                  key={index}
+                  index={index}
+                  updateBoard={updateBoard}
+                  >
+                  {square}
+                  </GameSquare>
+                )
+              })
+            }
+          </section>
 
-      <section className="flex justify-center w-fit my-3.75 mx-auto relative rounded-[10px]">
-        <GameSquare squareStyle={turnSquareStyle} isSelected={turn === TURNS.X}>{TURNS.X}</GameSquare>
-        <GameSquare squareStyle={turnSquareStyle} isSelected={turn === TURNS.O}>{TURNS.O}</GameSquare>
-      </section>
-      <button onClick={resetGame} className='w-40 py-2 px-3 m-3 bg-[#4f5030] font-bold text-xl text-[#dfd3bd] border-2 border-solid border-[#4f5030] rounded-lg transition-200 cursor-pointer hover:bg-[#dfd3bd] hover:text-[#4f5030]'>Reset game</button>
-      
-      <WinnerModal winner={winner} resetGame={resetGame}></WinnerModal>
-    </main>
+          <section className="flex justify-center w-fit my-3.75 mx-auto relative rounded-[10px]">
+            <GameSquare squareStyle={turnSquareStyle} isSelected={turn === TURNS.X}>{TURNS.X}</GameSquare>
+            <GameSquare squareStyle={turnSquareStyle} isSelected={turn === TURNS.O}>{TURNS.O}</GameSquare>
+          </section>
+          <button onClick={resetGame} className='w-40 py-2 px-3 m-3 bg-[#4f5030] font-bold text-xl text-[#dfd3bd] border-2 border-solid border-[#4f5030] rounded-lg transition-200 cursor-pointer hover:bg-[#dfd3bd] hover:text-[#4f5030]'>Reset game</button>
+          
+          <WinnerModal winner={winner} resetGame={resetGame}></WinnerModal>
+        </section>
+      </main>
+    </>
   )
 }
 
